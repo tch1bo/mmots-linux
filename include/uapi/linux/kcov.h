663c2a30de845dd13cf3cf64c3dfd437295d5ce2
Victor Chibotaru <tchibo@google.com>
2017-08-29 17:03:44 +0200

    kcov: support comparison operands collection
    Enables kcov to collect comparison operands from instrumented code.
    This is done by using Clang's -fsanitize=trace-cmp instrumentation
    (currently not available for GCC).
    
    The comparison operands help a lot in fuzz testing. E.g. they are
    used in Syzkaller to cover the interiors of conditional statements
    with way less attempts and thus make previously unreachable code
    reachable.
    
    To allow separate collection of coverage and comparison operands two
    different work modes are implemented. Mode selection is now done via
    a KCOV_ENABLE ioctl call with corresponding argument value.
    
    Signed-off-by: Victor Chibotaru <tchibo@google.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Alexander Popov <alex.popov@linux.com>
    Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Vegard Nossum <vegard.nossum@oracle.com>
    Cc: Quentin Casasnovas <quentin.casasnovas@oracle.com>
    Cc: syzkaller@googlegroups.com
    Cc: linux-mm@kvack.org
    Cc: linux-kernel@vger.kernel.org
    ---
    Clang instrumentation:
    https://clang.llvm.org/docs/SanitizerCoverage.html#tracing-data-flow
    Syzkaller:
    https://github.com/google/syzkaller

--- include/uapi/linux/kcov.h
+++ include/uapi/linux/kcov.h
@@ -7,4 +7,36 @@
 #define KCOV_ENABLE			_IO('c', 100)
 #define KCOV_DISABLE			_IO('c', 101)
 
+enum {
+	/*
+	 * Tracing coverage collection mode.
+	 * Covered PCs are collected in a per-task buffer.
+	 * In new KCOV version the mode is chosen by calling
+	 * ioctl(fd, KCOV_ENABLE, mode). In older versions the mode argument
+	 * was supposed to be 0 in such a call. So, for reasons of backward
+	 * compatibility, we have chosen the value KCOV_TRACE_PC to be 0.
+	 */
+	KCOV_TRACE_PC = 0,
+	/* Collecting comparison operands mode. */
+	KCOV_TRACE_CMP = 1,
+};
+
+/*
+ * Defines the format for the types of collected comparisons.
+ */
+enum kcov_cmp_type {
+	/*
+	 * LSB shows whether one of the arguments is a compile-time constant.
+	 */
+	KCOV_CMP_CONST = 1,
+	/*
+	 * Second and third LSBs contain the size of arguments (1/2/4/8 bytes).
+	 */
+	KCOV_CMP_SIZE1 = 0,
+	KCOV_CMP_SIZE2 = 2,
+	KCOV_CMP_SIZE4 = 4,
+	KCOV_CMP_SIZE8 = 6,
+	KCOV_CMP_SIZE_MASK = 6,
+};
+
 #endif /* _LINUX_KCOV_IOCTLS_H */
